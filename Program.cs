acb7b9fb1a2e280545f16d1cb272ed5d9f1bcefd
gabri <gabrielvilela.acer@gmail.com>
2023-07-17 19:41:57 -0400

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+
+
+namespace Bee1044 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, N;
+            string[] vet = (Console.ReadLine().Split(' '));
+            A = int.Parse((vet[0]));
+            B = int.Parse((vet[1]));
+
+
+            if (A % B == 0 ||  B % A == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
